5aafeaedce17b4bfdd6cac40f487c3e31d8f4878
NexoHost Corporation <96266038+NexoHost@users.noreply.github.com>
2025-06-19 16:26:40 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -8,10 +8,10 @@ CYAN="\033[1;36m"
 RESET="\033[0m"
 BOLD="\033[1m"
 
-# Mostrar banner limpio con interpretación de colores
-echo -e "${CYAN}============================================================"
-echo -e "${BOLD}${CYAN}         🚀  Welcome to NexoHost.es       ${RESET}${CYAN}"
-echo -e "============================================================${RESET}"
+# Mostrar banner limpio
+echo "${CYAN}============================================================"
+echo "${BOLD}${CYAN}         🚀  Bienvenido a la plataforma de NexoHost.es       ${RESET}${CYAN}"
+echo "============================================================${RESET}"
 
 # Funciones de log
 log_success() {
@@ -56,9 +56,9 @@ else
     exit 1
 fi
 
-echo -e "${CYAN}============================================================${RESET}"
+echo "${CYAN}============================================================${RESET}"
 log_success "Todos los servicios fueron iniciados correctamente. 🚀"
-echo -e "${CYAN}============================================================${RESET}"
+echo "${CYAN}============================================================${RESET}"
 
 # Mantener contenedor en ejecución
-tail -f /dev/null
+tail -f /dev/null asi me  lo diste
